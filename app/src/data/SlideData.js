0290a9c69ad3b35faa95693971cfa6298bb6d383
dogtwelve <x3191@hotmail.com>
2015-04-08 18:16:23 +0800

    clean data

--- app/src/data/SlideData.js
+++ app/src/data/SlideData.js
@@ -1,14 +1,11 @@
 define(function(require, exports, module) {
     var SlideData = {
-        userId: '109813050055185479846',
-        albumId: '6013105701911614529',
-        picasaUrl: 'https://picasaweb.google.com/data/feed/api/user/',
-        queryParams: '?alt=json&hl=en_US&access=visible&fields=entry(id,media:group(media:content,media:description,media:keywords,media:title))',
-        defaultImage: 'https://lh4.googleusercontent.com/-HbYp2q1BZfQ/U3LXxmWoy7I/AAAAAAAAAJk/VqI5bGooDaA/s1178-no/1.jpg'
+        queryDataUrl:'',
+        defaultImage: './assets/loading_img.gif'
     };
 
     SlideData.getUrl = function() {
-        return SlideData.picasaUrl + SlideData.userId + '/albumid/' + SlideData.albumId + SlideData.queryParams;
+        return SlideData.queryDataUrl;
     };
 
     SlideData.parse = function(data) {
